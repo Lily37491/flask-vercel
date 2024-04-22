a5054b8a8cc15590c1ffc7b429d8dfc7c365aa38
Payal@git <125844585+Lily37491@users.noreply.github.com>
2024-04-22 23:48:18 +0530

    Add files via upload

--- index.py
+++ index.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, Response
 import trafilatura
 import csv
 import textwrap
 
-app = Flask(__name__, template_folder=r'C:\Users\payal\flask-vercel\templates')
+
+app = Flask(__name__, template_folder=r'C:\Users\payal\flask-vercel\api\templates')
 
 #Define max-width for text-wrapping
 max_width = 80
 
+@app.route('/favicon.ico')
+def favicon():
+    return Response("", status=404)\
+    
 @app.route("/")
 def hello():
     return render_template("index.html")
@@ -17,7 +22,7 @@ def extract_text():
     url = request.form['url']
     download = trafilatura.fetch_url(url)
     text = trafilatura.extract(download)
-    return render_template(r'display-text.html', text=text)
+    return render_template(r'display.html', text=text)
 
 
 if __name__ == "__main__":
